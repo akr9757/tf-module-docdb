17a8206f47eb03ab9b2a7eb871f924596c0a9377
akr9757 <anilkumareddy9757@gmail.com>
2023-10-11 11:13:03 +0530

    terraform

---
+++ vars.tf
@@ -0,0 +1,13 @@
+variable "tags" {}
+variable "name" {
+  default = "docdb"
+}
+variable "env" {}
+variable "subnets" {}
+variable "vpc_id" {}
+variable "allow_db_cidr" {}
+variable "port_no" {
+  default = 27017
+}
+variable "engine_version" {}
+variable "kms_arn" {}
